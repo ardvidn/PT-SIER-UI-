57515c913949cd2578f90bc3d5f554fb7b785c5d
ardvidn <ardvidn93@gmail.com>
2025-02-09 00:58:20 +0700

    developing front end

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,13 +1,5 @@
 "use client";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  LayersControl,
-  LayerGroup,
-  GeoJSON,
-} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, LayersControl, LayerGroup, GeoJSON, ZoomControl } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import point from "../testdata/contohtitik";
 import L from "leaflet";
@@ -27,9 +19,7 @@ const Maps = () => {
     // Fetch data dari API
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:8070/api/convert/bataskelurahan"
-        );
+        const response = await axios.get("http://localhost:8070/api/convert/bataskelurahan");
         setGeoJsonData(response.data.data); // Simpan data GeoJSON ke state
       } catch (error) {
         console.error("Error fetching GeoJSON data:", error);
@@ -54,47 +44,36 @@ const Maps = () => {
           center={[-0.654744, 114.569528]}
           zoom={13}
           scrollWheelZoom={true}
+          zoomControl={false}
         >
+          <Navbar />
+          {/* <SearchBar /> */}
           {/* <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
           /> */}
+
           <LayersControl>
             <LayersControl.BaseLayer name="Open Street Map">
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
+              <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             </LayersControl.BaseLayer>
 
             <LayersControl.BaseLayer checked name="Google Map">
-              <TileLayer
-                attribution="Google Maps"
-                url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
-              />
+              <TileLayer attribution="Google Maps" url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}" />
             </LayersControl.BaseLayer>
 
             <LayersControl.BaseLayer name="Google Map Satellite">
               <LayerGroup>
-                <TileLayer
-                  attribution="Google Maps Satellite"
-                  url="https://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}"
-                />
+                <TileLayer attribution="Google Maps Satellite" url="https://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}" />
                 <TileLayer url="https://www.google.cn/maps/vt?lyrs=y@189&gl=cn&x={x}&y={y}&z={z}" />
               </LayerGroup>
             </LayersControl.BaseLayer>
           </LayersControl>
-
-          {geoJsonData && (
-            <GeoJSON data={geoJsonData} onEachFeature={onEachFeatureKec} />
-          )}
+          <ZoomControl position="topright" />
+          {geoJsonData && <GeoJSON data={geoJsonData} onEachFeature={onEachFeatureKec} />}
           {test.map((item, index) => (
-            <Marker
-              key={index}
-              position={[item.LATTITUDE, item.LONGITUDE]}
-              icon={icon}
-            >
+            <Marker key={index} position={[item.LATTITUDE, item.LONGITUDE]} icon={icon}>
               <Popup>
                 A pretty CSS3 popup. <br /> Easily Customizable. {item.judul}
               </Popup>
@@ -102,17 +81,6 @@ const Maps = () => {
           ))}
           {/* <UploadButtons /> */}
         </MapContainer>
-        <div
-          className="absolute bottom-2 z-[1000]"
-          // style={{
-          //   position: "absolute",
-          //   top: "10px",
-          //   right: "10px",
-          //   zIndex: 1000, // Pastikan zIndex lebih tinggi dari peta
-          // }}
-        >
-          {/* <UploadMain /> */}
-        </div>
       </div>
     </>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,119 +1,121 @@
-"use client";
-import { svgIconClasses } from "@mui/material";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
-import TemporaryDrawer from "@/components/TemporaryDrawer";
-import BasicModal from "@/components/BasicModal";
+import React, { useState } from "react";
+import { TextField, IconButton, Button, Modal, Box } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+
+const mockData = {
+  "74.08.060.012.001.0039.0": {
+    wajibPajak: "NUR HAYATI",
+    alamat: "JL POROS BATU PUTIH DUSUN I",
+    rtRw: "/",
+    dusun: "",
+    kecamatan: "BATU PUTIH",
+    kelurahan: "",
+  },
+};
 
 const Navbar = () => {
-  const [isClick, setIsCLick] = useState(false);
+  const [showMore, setShowMore] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [openModal, setOpenModal] = useState(false);
+  const [propertyData, setPropertyData] = useState(null);
 
-  const toggleNavbar = () => {
-    setIsCLick(!isClick);
+  const formatNop = (value: any) => {
+    // Hapus semua karakter non-digit
+    let numbersOnly = value.replace(/\D/g, "");
+
+    // Pastikan panjang maksimal 18 digit
+    numbersOnly = numbersOnly.slice(0, 18);
+
+    // Format sesuai pola "XX.XX.XXX.XXX.XXX.XXXX.X"
+    let formatted = numbersOnly
+      .replace(/^(\d{2})(\d{0,2})/, "$1.$2") // 2 digit pertama + titik + 2 digit berikutnya
+      .replace(/^(\d{2})\.(\d{2})(\d{0,3})/, "$1.$2.$3") // 2 digit + titik + 2 digit + titik + 3 digit
+      .replace(/^(\d{2})\.(\d{2})\.(\d{3})(\d{0,3})/, "$1.$2.$3.$4") // tambah 3 digit + titik + 3 digit
+      .replace(/^(\d{2})\.(\d{2})\.(\d{3})\.(\d{3})(\d{0,3})/, "$1.$2.$3.$4.$5") // tambah 3 digit + titik + 3 digit
+      .replace(/^(\d{2})\.(\d{2})\.(\d{3})\.(\d{3})\.(\d{3})(\d{0,4})/, "$1.$2.$3.$4.$5.$6") // tambah 4 digit + titik + 1 digit
+      .replace(/^(\d{2})\.(\d{2})\.(\d{3})\.(\d{3})\.(\d{3})\.(\d{4})(\d{0,1})/, "$1.$2.$3.$4.$5.$6.$7"); // tambah 1 digit terakhir
+
+    return formatted;
+  };
+
+  const handleSearchChange = (e) => {
+    const inputValue = e.target.value;
+
+    // Jika pengguna menghapus karakter, biarkan penghapusan terjadi
+    if (e.nativeEvent.inputType === "deleteContentBackward") {
+      setSearchQuery(inputValue);
+      return;
+    }
+
+    // Format input jika pengguna mengetik
+    const formattedNop = formatNop(inputValue);
+    setSearchQuery(formattedNop);
+  };
+
+  const handleSearch = () => {
+    if (mockData[searchQuery]) {
+      setPropertyData(mockData[searchQuery]);
+      setOpenModal(true);
+    } else {
+      alert("Data tidak ditemukan");
+    }
   };
 
   return (
-    <>
-      <nav className="bg-black ">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <TemporaryDrawer />
-              <div className="flex-shrink-0">
-                <Link href="" className="text-white">
-                  LOGO
-                </Link>
-              </div>
-            </div>
-            <div className="hidden md:block">
-              <div className="ml-4 flex items-center space-x-4">
-                <Link
-                  href=""
-                  className="text-white hover:bg-sky-700 hover:text-white rounded-2xl p-2"
-                >
-                  Home
-                </Link>
-                <Link
-                  href=""
-                  className="text-white hover:bg-sky-700 hover:text-white rounded-2xl p-2"
-                >
-                  Product
-                </Link>
-                <Link
-                  href=""
-                  className="text-white hover:bg-sky-700 hover:text-white rounded-2xl p-2"
-                >
-                  About Us
-                </Link>
-              </div>
-            </div>
-            <div className="md:hidden flex items-center">
-              <button
-                className="inline-flex items-center justify-center p-2 rounded-md text-white  hover:text-white"
-                onClick={toggleNavbar}
-              >
-                {isClick ? (
-                  <svg
-                    className="h-6 m-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M6 18L18 6M6 6L12 12"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="h-6 m-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M4 6h16M4 12h16m-7 6h7"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
-          </div>
+    <div className="p-4 bg-white shadow-md rounded-md max-w-md mx-auto absolute top-4 left-4 z-[1000]">
+      <div className="flex items-center space-x-2 border p-2 rounded-md">
+        <MenuIcon />
+        <TextField
+          fullWidth
+          variant="standard"
+          placeholder="Search NOP"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          inputProps={{ maxLength: 24 }} // Maksimal 24 karakter untuk format lengkap
+        />
+        <IconButton onClick={handleSearch}>
+          <SearchIcon />
+        </IconButton>
+      </div>
+
+      <Button fullWidth endIcon={showMore ? <ExpandLessIcon /> : <ExpandMoreIcon />} onClick={() => setShowMore(!showMore)}>
+        {showMore ? "Show Less" : "Show More"}
+      </Button>
+
+      {showMore && (
+        <div className="mt-4 p-4 border rounded-md">
+          <p>Wajib Pajak: </p>
+          <p>Alamat Objek: </p>
+          <p>RT/RW: /</p>
+          <p>Dusun: </p>
+          <p>Kecamatan: </p>
+          <p>Kelurahan: </p>
         </div>
-        {isClick && (
-          <div className="md:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <Link
-                href=""
-                className="text-white block hover:bg-sky-700 hover:text-white rounded-2xl p-2"
-              >
-                Home
-              </Link>
-              <Link
-                href=""
-                className="text-white block hover:bg-sky-700 hover:text-white rounded-2xl p-2"
-              >
-                Home
-              </Link>
-              <Link
-                href=""
-                className="text-white block hover:bg-sky-700 hover:text-white rounded-2xl p-2"
-              >
-                Home
-              </Link>
+      )}
+
+      {/* Modal Detail NOP */}
+      <Modal open={openModal} onClose={() => setOpenModal(false)}>
+        <Box className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-md shadow-md">
+          <h2 className="text-lg font-bold mb-2">{searchQuery}</h2>
+          {propertyData && (
+            <div>
+              <p>Wajib Pajak: {propertyData.wajibPajak}</p>
+              <p>Alamat Objek: {propertyData.alamat}</p>
+              <p>RT/RW: {propertyData.rtRw}</p>
+              <p>Dusun: {propertyData.dusun}</p>
+              <p>Kecamatan: {propertyData.kecamatan}</p>
+              <p>Kelurahan: {propertyData.kelurahan}</p>
             </div>
-          </div>
-        )}
-      </nav>
-    </>
+          )}
+          <Button fullWidth onClick={() => setOpenModal(false)} className="mt-4">
+            Close
+          </Button>
+        </Box>
+      </Modal>
+    </div>
   );
 };
 
